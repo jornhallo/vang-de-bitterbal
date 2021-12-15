942747e2bd0d34aa85614ae403c0f1443fcf071a
JORN <95621941+jornhallo@users.noreply.github.com>
2021-12-15 09:45:32 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -4,21 +4,45 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     speler.change(LedSpriteProperty.X, 1)
 })
+let kroket: game.LedSprite = null
 let pauze = 0
 let bitterbal: game.LedSprite = null
+let x = 0
+let getal = 0
 let speler: game.LedSprite = null
 speler = game.createSprite(2, 4)
 basic.forever(function () {
-    bitterbal = game.createSprite(randint(0, 4), 0)
-    for (let index = 0; index < 4; index++) {
-        basic.pause(500)
-        pauze += 100
-        bitterbal.change(LedSpriteProperty.Y, 1)
-    }
-    if (bitterbal.isTouching(speler)) {
-        game.addScore(1)
-        bitterbal.delete()
+    getal = randint(1, 4)
+    if (getal < 4) {
+        x = randint(0, 4)
+        bitterbal = game.createSprite(x, 1)
+        for (let index = 0; index < 3; index++) {
+            basic.pause(500)
+            pauze += 100
+            bitterbal.change(LedSpriteProperty.Y, 1)
+        }
+        if (bitterbal.isTouching(speler)) {
+            game.addScore(1)
+            bitterbal.delete()
+        } else {
+            game.gameOver()
+        }
     } else {
-        game.gameOver()
+        x = randint(0, 4)
+        kroket = game.createSprite(x, 0)
+        bitterbal = game.createSprite(x, 1)
+        for (let index = 0; index < 3; index++) {
+            basic.pause(400)
+            pauze += 100
+            kroket.change(LedSpriteProperty.Y, 1)
+            bitterbal.change(LedSpriteProperty.Y, 1)
+        }
+        if (bitterbal.isTouching(speler)) {
+            game.addScore(2)
+            kroket.delete()
+            bitterbal.delete()
+        } else {
+            game.gameOver()
+        }
     }
 })
